4d9278f2c29f2dd87cfdb0cb606f9ab988589a4a
NiketaJen <niketa.jenerette@gmail.com>
2020-07-19 21:45:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(event) {
+    changeText();
+
+    function changeText() {
+    let body = document.querySelector("body")
+      let newP = document.getElementById("text")
+      newP.innerHTML = "This is really cool!"
+      body.appendChild("newP")
+    }
+  });
